aa53b23a0628b61721a78ad75f9e40d500867f01
Caleb Bolaños Ramos <bolanos.c@hotmail.com>
2022-03-14 21:16:35 -0600

    validaciones

--- src/views/practicas/Practica1.vue
+++ src/views/practicas/Practica1.vue
@@ -11,64 +11,73 @@
           </v-card-title>
           <v-card-text>
             <p class="text--primary mb-1">
-              Para cifrar debes de especificar los siguientes parametros:
+              Para cifrar, debes de especificar los siguientes parametros:
             </p>
           </v-card-text>
-          <v-card-text>
-            <v-radio-group v-model="alfabetoCifrar" row>
-              <template v-slot:label>
-                <div>Alfabeto:</div>
-              </template>
-              <v-radio label="Inglés (26)" value="1"> </v-radio>
-              <v-radio label="Español (27)" value="2"> </v-radio>
-            </v-radio-group>
-            <v-row>
-              <v-col cols="6">
-                <v-text-field
-                  v-model="alphaCifrar"
-                  prepend-inner-icon="mdi-close-circle"
-                  label="Factor multiplicativo (Alpha)"
-                  type="number"
-                  outlined
-                ></v-text-field>
-              </v-col>
-              <v-col cols="6">
-                <v-text-field
-                  v-model="betaCifrar"
-                  prepend-inner-icon="mdi-plus-circle"
-                  label="Corrimiento (beta)"
-                  type="number"
-                  outlined
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-file-input
-              label="Archivo a cifrar"
-              outlined
-              accept=".txt"
-              @change="leerArchivoCifrar"
-            ></v-file-input>
-            <v-textarea
-              v-model="plaintext"
-              label="Contenido del archivo a cifrar"
-              auto-grow
-              rows="3"
-              outlined
-              readonly
-            ></v-textarea>
-            
+          <v-form
+            ref="formCifrar"
+            v-model="valid"
+            lazy-validation
+            @submit.prevent
+          >
+            <v-card-text>
+              <v-radio-group v-model="alfabetoCifrar" row>
+                <template v-slot:label>
+                  <div>Alfabeto:</div>
+                </template>
+                <v-radio label="Inglés (26)" value="1"> </v-radio>
+                <v-radio label="Español (27)" value="2"> </v-radio>
+              </v-radio-group>
+              <v-row>
+                <v-col cols="6">
+                  <v-text-field
+                    v-model="alphaCifrar"
+                    prepend-inner-icon="mdi-close-circle"
+                    label="Factor multiplicativo (Alpha)"
+                    type="number"
+                    outlined
+                    :rules="reglasAlfa"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="6">
+                  <v-text-field
+                    v-model="betaCifrar"
+                    prepend-inner-icon="mdi-plus-circle"
+                    label="Corrimiento (beta)"
+                    type="number"
+                    outlined
+                    :rules="reglasBeta"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-file-input
+                label="Archivo a cifrar"
+                outlined
+                accept=".txt"
+                @change="leerArchivoCifrar"
+                :rules="reglasArchivo"
+              ></v-file-input>
+              <v-textarea
+                v-model="plaintext"
+                label="Contenido del archivo a cifrar"
+                auto-grow
+                rows="3"
+                outlined
+                readonly
+              ></v-textarea>
 
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn type="submit" block color="primary" @click="verificar()"
-                >Cifrar</v-btn
-              >
-              <v-spacer></v-spacer>
-            </v-card-actions>
-            <span class="d-flex align-center justify-center flex-wrap">
-              <span class="me-2">{{ mensajeCifrado }}</span>
-            </span>
-          </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn type="submit" block color="primary" @click="verificar()"
+                  >Cifrar</v-btn
+                >
+                <v-spacer></v-spacer>
+              </v-card-actions>
+              <span class="d-flex align-center justify-center flex-wrap">
+                <span class="me-2">{{ mensajeCifrado }}</span>
+              </span>
+            </v-card-text>
+          </v-form>
         </v-card>
       </v-col>
       <v-col cols="12" lg="6" md="6" sm="6">
@@ -78,69 +87,79 @@
           </v-card-title>
           <v-card-text>
             <p class="text--primary mb-1">
-              Para descifrar debes de especificar los siguientes parametros:
+              Para descifrar, debes de especificar los siguientes parametros:
             </p>
           </v-card-text>
-          <v-card-text>
-            <v-radio-group v-model="alfabetoDescifrar" row>
-              <template v-slot:label>
-                <div>Alfabeto:</div>
-              </template>
-              <v-radio label="Inglés (26)" value="1"> </v-radio>
-              <v-radio label="Español (27)" value="2"> </v-radio>
-            </v-radio-group>
-            <v-row>
-              <v-col cols="6">
-                <v-text-field
-                  v-model="alphaDescifrar"
-                  prepend-inner-icon="mdi-close-circle"
-                  label="Factor multiplicativo (Alpha)"
-                  type="number"
-                  outlined
-                ></v-text-field>
-              </v-col>
-              <v-col cols="6">
-                <v-text-field
-                  v-model="betaDescifrar"
-                  prepend-inner-icon="mdi-plus-circle"
-                  label="Corrimiento (beta)"
-                  type="number"
-                  outlined
-                ></v-text-field>
-              </v-col>
-            </v-row>
+          <v-form
+            ref="formDescifrar"
+            v-model="valid"
+            lazy-validation
+            @submit.prevent
+          >
+            <v-card-text>
+              <v-radio-group v-model="alfabetoDescifrar" row>
+                <template v-slot:label>
+                  <div>Alfabeto:</div>
+                </template>
+                <v-radio label="Inglés (26)" value="1"> </v-radio>
+                <v-radio label="Español (27)" value="2"> </v-radio>
+              </v-radio-group>
+              <v-row>
+                <v-col cols="6">
+                  <v-text-field
+                    v-model="alphaDescifrar"
+                    prepend-inner-icon="mdi-close-circle"
+                    label="Factor multiplicativo (Alpha)"
+                    type="number"
+                    outlined
+                    :rules="reglasAlfa"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="6">
+                  <v-text-field
+                    v-model="betaDescifrar"
+                    prepend-inner-icon="mdi-plus-circle"
+                    label="Corrimiento (beta)"
+                    type="number"
+                    outlined
+                    :rules="reglasBeta"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
 
-            <v-file-input
-              label="Archivo a descifrar"
-              outlined
-              accept=".txt"
-              @change="leerArchivoDescifrar"
-            ></v-file-input>
+              <v-file-input
+                label="Archivo a descifrar"
+                outlined
+                accept=".txt"
+                @change="leerArchivoDescifrar"
+                :rules="reglasArchivo"
+              ></v-file-input>
 
-            <v-textarea
-              v-model="cifrado"
-              label="Contenido del archivo a descifrar"
-              auto-grow
-              rows="3"
-              readonly
-              outlined
-            ></v-textarea>
+              <v-textarea
+                v-model="cifrado"
+                label="Contenido del archivo a descifrar"
+                auto-grow
+                rows="3"
+                readonly
+                outlined
+              ></v-textarea>
 
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                type="submit"
-                block
-                color="primary"
-                @click="verificarDescifrado()"
-                >Descifrar</v-btn
-              >
-              <v-spacer></v-spacer>
-            </v-card-actions>
-            <span class="d-flex align-center justify-center flex-wrap">
-              <span class="me-2">{{ mensajeDescifrado }}</span>
-            </span>
-          </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  type="submit"
+                  block
+                  color="primary"
+                  @click="verificarDescifrado()"
+                  >Descifrar</v-btn
+                >
+                <v-spacer></v-spacer>
+              </v-card-actions>
+              <span class="d-flex align-center justify-center flex-wrap">
+                <span class="me-2">{{ mensajeDescifrado }}</span>
+              </span>
+            </v-card-text>
+          </v-form>
         </v-card>
       </v-col>
     </v-row>
@@ -221,53 +240,73 @@ export default {
     mensajeDescifrado: "",
 
     contenidoArchivo: null,
+
+    valid: true,
+
+    reglasAlfa: [
+      (v) => !!v || "Este campo es requerido",
+      (v) => (v && v > 0 && v < 27) || "Introduce un alfa valido",
+    ],
+    reglasBeta: [
+      (v) => !!v || "Este campo es requerido",
+      (v) => (v && v > 0) || "Introduce un beta valido",
+    ],
+
+    reglasArchivo: [
+      (v) => !!v || "El archivo es requerido",
+      (v) => (v && v.size > 0) || "El archivo es requerido",
+    ],
   }),
 
   methods: {
     verificar() {
-      let alfabeto = this.obtenerAlfabeto(this.alfabetoCifrar);
-      if (this.MCDeuclides(this.alphaCifrar, alfabeto.length) === 1) {
-        //pasamos el mensaje a minusculas y quitamos los espacios
-        this.plaintext = this.contenidoArchivo;
-        console.log(this.plaintext);
+      if (this.$refs.formCifrar.validate()) {
+        let alfabeto = this.obtenerAlfabeto(this.alfabetoCifrar);
+        if (this.MCDeuclides(this.alphaCifrar, alfabeto.length) === 1) {
+          //pasamos el mensaje a minusculas y quitamos los espacios
+          this.plaintext = this.contenidoArchivo;
+          console.log(this.plaintext);
 
-        let texto = this.plaintext.toUpperCase().replace(/\s+/g, "");
+          let texto = this.plaintext.toUpperCase().replace(/\s+/g, "");
 
-        this.mensajeCifrado = this.cifrar(
-          parseInt(this.alphaCifrar),
-          parseInt(this.betaCifrar),
-          texto,
-          alfabeto
-        );
-        this.crearArchivo("cifrado.txt", this.mensajeCifrado);
-      } else {
-        alert(
-          "El factor multiplicativo de alfa no es valido ya que no es coprimo. Intenta con otro valor"
-        );
+          this.mensajeCifrado = this.cifrar(
+            parseInt(this.alphaCifrar),
+            parseInt(this.betaCifrar),
+            texto,
+            alfabeto
+          );
+          this.crearArchivo("cifrado.txt", this.mensajeCifrado);
+        } else {
+          alert(
+            "El factor multiplicativo de alfa no es valido ya que no es coprimo. Intenta con otro valor"
+          );
+        }
       }
     },
 
     verificarDescifrado() {
-      let alfabeto = this.obtenerAlfabeto(this.alfabetoDescifrar);
-      if (this.MCDeuclides(this.alphaDescifrar, alfabeto.length) === 1) {
-        //pasamos el mensaje a minusculas y quitamos los espacios
-        let alfaInversa = this.inversoMultiplicativoModular(
-          parseInt(this.alphaDescifrar),
-          alfabeto.length
-        );
-        console.log(alfaInversa);
-        this.cifrado = this.contenidoArchivo;
-        this.mensajeDescifrado = this.descifrar(
-          alfaInversa,
-          parseInt(this.betaDescifrar),
-          this.cifrado,
-          alfabeto
-        );
-        this.crearArchivo("descifrado.txt", this.mensajeDescifrado);
-      } else {
-        alert(
-          "El inverso multiplicativo no existe para el valor de alfa dado. Intenta con otro valor"
-        );
+      if (this.$refs.formDescifrar.validate()) {
+        let alfabeto = this.obtenerAlfabeto(this.alfabetoDescifrar);
+        if (this.MCDeuclides(this.alphaDescifrar, alfabeto.length) === 1) {
+          //pasamos el mensaje a minusculas y quitamos los espacios
+          let alfaInversa = this.inversoMultiplicativoModular(
+            parseInt(this.alphaDescifrar),
+            alfabeto.length
+          );
+          console.log(alfaInversa);
+          this.cifrado = this.contenidoArchivo;
+          this.mensajeDescifrado = this.descifrar(
+            alfaInversa,
+            parseInt(this.betaDescifrar),
+            this.cifrado,
+            alfabeto
+          );
+          this.crearArchivo("descifrado.txt", this.mensajeDescifrado);
+        } else {
+          alert(
+            "El inverso multiplicativo no existe para el valor de alfa dado. Intenta con otro valor"
+          );
+        }
       }
     },
 
@@ -344,7 +383,7 @@ export default {
     leerArchivoCifrar(archivo) {
       if (!archivo) {
         this.contenidoArchivo = "No File Chosen";
-        return
+        return;
       }
       let reader = new FileReader();
 
@@ -353,14 +392,14 @@ export default {
       reader.readAsText(archivo);
       reader.onload = () => {
         this.contenidoArchivo = reader.result;
-        this.plaintext = this.contenidoArchivo
+        this.plaintext = this.contenidoArchivo;
       };
     },
 
     leerArchivoDescifrar(archivo) {
       if (!archivo) {
         this.contenidoArchivo = "No File Chosen";
-        return
+        return;
       }
       let reader = new FileReader();
 
@@ -369,7 +408,7 @@ export default {
       reader.readAsText(archivo);
       reader.onload = () => {
         this.contenidoArchivo = reader.result;
-        this.cifrado = this.contenidoArchivo
+        this.cifrado = this.contenidoArchivo;
       };
     },
 
