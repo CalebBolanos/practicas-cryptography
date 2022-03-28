aac8c13b90a73cef525a3c452117594649e39f81
Caleb Bolaños Ramos <bolanos.c@hotmail.com>
2022-03-28 16:42:15 -0600

    cifrado funcionando, falta descifrado aun

--- src/views/practicas/Practica1.vue
+++ src/views/practicas/Practica1.vue
@@ -38,26 +38,26 @@
               <v-file-input
                 label="Archivo de entrada"
                 outlined
-                accept=".txt"
+                accept=".bmp"
                 @change="leerArchivoCifrar"
                 :rules="reglasArchivo"
               ></v-file-input>
               <v-row>
                 <v-col cols="6">
                   <v-text-field
-                    v-model="llave"
+                    v-model="llaveString"
                     prepend-inner-icon="mdi-key"
-                    label="Llave (16 bits)"
-                    type="number"
+                    label="Llave (16 bytes)"
+                    type="text"
                     outlined
                   ></v-text-field>
                 </v-col>
                 <v-col cols="6">
                   <v-text-field
-                    v-model="c0"
+                    v-model="c0String"
                     prepend-inner-icon="mdi-code-array"
                     label="Vector de inicializacion (C0)"
-                    type="number"
+                    type="text"
                     outlined
                   ></v-text-field>
                 </v-col>
@@ -70,7 +70,7 @@
                   block
                   color="primary"
                   @click="verificarCampos()"
-                  >{{modo}}</v-btn
+                  >{{ modo }}</v-btn
                 >
                 <v-spacer></v-spacer>
               </v-card-actions>
@@ -86,6 +86,7 @@
 </template>
 
 <script>
+import CryptoJS from "crypto-js";
 export default {
   name: "PracticaUno",
   data: () => ({
@@ -93,88 +94,169 @@ export default {
     modo: "cifrar",
     valid: true,
     modoOperacion: "ECB",
-    llave: null,
-    c0: null,
+    llaveString: "1234123412ABCDEF",
+    c0String: "ABCDEF1234123412",
     nombreArchivo: "",
     contenidoArchivo: null,
 
+    //variables para cifrado-descifrado
+    llave: null,
+    c0: null,
 
     reglasArchivo: [
       (v) => !!v || "El archivo es requerido",
       (v) => (v && v.size > 0) || "El archivo es requerido",
     ],
-    
-    plaintext: "",
+
+    plaintext: null,
     mensajeCifrado: "",
-
-    
-
-    
-
-    
   }),
 
   methods: {
     verificarCampos() {
       if (this.$refs.formAES.validate()) {
         console.log("ok");
+
+        this.llave = CryptoJS.enc.Utf8.parse(this.llaveString);
+        this.c0 = CryptoJS.enc.Utf8.parse(this.c0String);
+
+        //dependiendo de la imagen el tamaño de su header cambiara
+        let headerBMP = this.contenidoArchivo.slice(0, 124);
+        let contenidoBMP = this.contenidoArchivo.slice(
+          125,
+          this.contenidoArchivo.byteLength
+        );
+
+        if (this.modo === "cifrar") {
+          this.cifrarAES(
+            headerBMP,
+            contenidoBMP,
+            this.llave,
+            this.c0,
+            this.modoOperacion
+          );
+        } else {
+          this.descifrarAES(
+            headerBMP,
+            contenidoBMP,
+            this.llave,
+            this.c0,
+            this.modoOperacion
+          );
+        }
       }
+    },
+
+    cifrarAES(header, input, llave, c0, modoOperacion) {
+      console.log(modoOperacion);
+
+      let wordArray = CryptoJS.lib.WordArray.create(input);
+      let cifrado = CryptoJS.AES.encrypt(wordArray, llave, {
+        iv: c0,
+        mode: CryptoJS.mode.CBC,
+      });
+
+      let contenidoArchivo = new Blob([header, cifrado.toString()]);
+
+      this.crearArchivo(this.nombreArchivo + "_eMODO.bmp", contenidoArchivo);
+    },
+
+    descifrarAES(header, input, llave, c0, modoOperacion) {
+      console.log(input, modoOperacion);
+
+      //let wordArray = CryptoJS.lib.WordArray.create();
+      //console.log(wordArray);
+
+      let blob = new Blob([input], { type: "text/plain" });
+      let reader = new FileReader();
+      reader.onload = () => {
+        let descifrado = CryptoJS.AES.decrypt(
+          reader.result,
+          llave,
+          {
+            iv: c0,
+            mode: CryptoJS.mode.CBC,
+          }
+        );
+        console.log(descifrado);
+        var typedArray = this.convertWordArrayToUint8Array(descifrado);
+        let archivoSalida = new Blob([header, typedArray]);
+
+        this.crearArchivo(this.nombreArchivo + "_dMODO.bmp", archivoSalida);
+      };
+      reader.readAsText(blob);
+
+      /** 
+      let reader = new FileReader();
+      reader.onload = () => {
+        
+      };
+      reader.readAsText(this.plaintext);
+      */
     },
 
     leerArchivoCifrar(archivo) {
+      //validacion de archivo de entrada
       if (!archivo) {
         this.contenidoArchivo = "No File Chosen";
         return;
       }
+      //obtenemos el nombre del archivo
       this.nombreArchivo = archivo.name.substr(
         0,
         archivo.name.lastIndexOf(".")
       );
-      //console.log(this.nombreArchivo)
 
       let reader = new FileReader();
-
-      reader.readAsText(archivo);
-      reader.onload = () => {
-        this.contenidoArchivo = reader.result;
-        this.plaintext = this.contenidoArchivo;
-      };
-    },
-
-    leerArchivoDescifrar(archivo) {
-      if (!archivo) {
-        this.contenidoArchivo = "No File Chosen";
-        return;
+      if (this.modo === "cifrar") {
+        reader.readAsArrayBuffer(archivo);
+      } else {
+        reader.readAsArrayBuffer(archivo);
       }
-      this.nombreArchivo = archivo.name.substr(
-        0,
-        archivo.name.lastIndexOf(".")
-      );
-      //console.log(this.nombreArchivo)
-
-      let reader = new FileReader();
-
-      reader.readAsText(archivo);
       reader.onload = () => {
         this.contenidoArchivo = reader.result;
-        this.cifrado = this.contenidoArchivo;
+        console.log(reader.result);
+        this.plaintext = archivo;
       };
     },
 
-    crearArchivo(nombreArchivo, texto) {
-      var element = document.createElement("a");
-      element.setAttribute(
-        "href",
-        "data:text/plain;charset=utf-8," + encodeURIComponent(texto)
-      );
+    crearArchivo(nombreArchivo, contenido) {
+      let element = document.createElement("a");
+
+      let url = window.URL.createObjectURL(contenido);
+      element.href = url;
       element.setAttribute("download", nombreArchivo);
 
       element.style.display = "none";
       document.body.appendChild(element);
 
       element.click();
-
+      window.URL.revokeObjectURL(url);
       document.body.removeChild(element);
+    },
+
+    convertWordArrayToUint8Array(wordArray) {
+      let arrayOfWords = Object.prototype.hasOwnProperty.call(
+        wordArray,
+        "words"
+      )
+        ? wordArray.words
+        : [];
+      let length = Object.prototype.hasOwnProperty.call(wordArray, "sigBytes")
+        ? wordArray.sigBytes
+        : arrayOfWords.length * 4;
+      let uInt8Array = new Uint8Array(length),
+        index = 0,
+        word,
+        i;
+      for (i = 0; i < length; i++) {
+        word = arrayOfWords[i];
+        uInt8Array[index++] = word >> 24;
+        uInt8Array[index++] = (word >> 16) & 0xff;
+        uInt8Array[index++] = (word >> 8) & 0xff;
+        uInt8Array[index++] = word & 0xff;
+      }
+      return uInt8Array;
     },
   },
 };
