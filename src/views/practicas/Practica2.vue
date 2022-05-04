469937882ed59de7de50848c18de4d1b5107f896
Caleb Bolaños Ramos <bolanos.c@hotmail.com>
2022-05-04 15:29:30 -0500

    documentacion del componente

--- src/views/practicas/Practica2.vue
+++ src/views/practicas/Practica2.vue
@@ -24,7 +24,8 @@
               <span class="text-h5">Generar llaves</span>
             </v-card-title>
             <v-card-text>
-              Puedes descargar las llaves generadas dando clic a sus respectivos botones
+              Puedes descargar las llaves generadas dando clic a sus respectivos
+              botones
             </v-card-text>
             <v-card-text>
               <v-container>
@@ -44,7 +45,7 @@
                       dark
                       block
                       @click="
-                        crearArchivo('publica.txt', llavePemPublicaGenerada)
+                        crearArchivoTxt('publica.txt', llavePemPublicaGenerada)
                       "
                     >
                       Descargar pública
@@ -65,7 +66,7 @@
                       dark
                       block
                       @click="
-                        crearArchivo('privada.txt', llavePemPrivadaGenerada)
+                        crearArchivoTxt('privada.txt', llavePemPrivadaGenerada)
                       "
                     >
                       Descargar privada
@@ -94,7 +95,10 @@
             <h2 class="text-2xl font-weight-semibold">Firma</h2>
           </v-card-title>
           <v-card-text>
-            <p class="text--primary mb-1">Puedes firmar un archivo de entrada dados los siguientes parámetros:</p>
+            <p class="text--primary mb-1">
+              Puedes firmar un archivo de entrada dados los siguientes
+              parámetros:
+            </p>
           </v-card-text>
           <v-form
             ref="formFirma"
@@ -126,7 +130,7 @@
                   type="submit"
                   block
                   color="primary"
-                  @click="verificarCampos()"
+                  @click="verificarCamposFirma()"
                   >Firmar Archivo</v-btn
                 >
                 <v-spacer></v-spacer>
@@ -163,7 +167,10 @@
             <h2 class="text-2xl font-weight-semibold">Verificación</h2>
           </v-card-title>
           <v-card-text>
-            <p class="text--primary mb-1">Ingresa los siguientes parámetros para verificar la firma digital y la integridad del mensaje de un archivo previamente firmado:</p>
+            <p class="text--primary mb-1">
+              Ingresa los siguientes parámetros para verificar la firma digital
+              y la integridad del mensaje de un archivo previamente firmado:
+            </p>
           </v-card-text>
           <v-form
             ref="formVerificacion"
@@ -213,6 +220,15 @@
 </template>
 
 <script>
+/**
+ * Practica 2
+ *
+ * Este componente contiene la implementación de una firma digital utilizando RSA
+ *
+ * El componente puede firmar y verificar archivos de texto que contengan un mensaje,
+ * ademas de generar llaves privadas y publicas que se pueden utilizar en el programa
+ *
+ */
 import rsa from "js-crypto-rsa";
 import keyutil from "js-crypto-key-utils";
 import { encode, decode } from "base64-arraybuffer";
@@ -220,27 +236,32 @@ import { encode, decode } from "base64-arraybuffer";
 export default {
   name: "PracticaDos",
   data: () => ({
+    //variables para generar llaves
     dialogLlaves: false,
     llavePemPublicaGenerada: "",
     llavePemPrivadaGenerada: "",
 
+    //variables usadas para generar la firma digital
     valid: true,
     llavePemPrivada: null,
     llavePrivada: null,
     archivoOriginal: null,
     nombreArchivo: "",
 
+    //variables para llevar a cabo la verificacion
     validVerificacion: true,
     llavePemPublica: null,
     llavePublica: null,
     archivoFirmado: null,
     firmaArchivo: null,
 
+    //variable para snackbar
     snackbar: false,
     infoSnackbar: "",
     colorSnackbar: "",
     timeout: 5000,
 
+    //validaciones de form
     reglasArchivo: [
       (v) => !!v || "El archivo es requerido",
       (v) => (v && v.size > 0) || "El archivo es requerido",
@@ -248,33 +269,37 @@ export default {
   }),
 
   methods: {
+    /**
+     * funcion generarLlaves()
+     * Funcion que genera llaves RSA de 1024 bits (publica y privada) para que
+     * puedan ser descargadas posteriormente en un archivo txt
+     */
     generarLlaves() {
       rsa.generateKey(1024).then(async (key) => {
         //primero se generan las llaves en formato JWK
         const publicKey = key.publicKey;
         const privateKey = key.privateKey;
 
-        //console.log(publicKey, privateKey);
-
         let keyPublicObjFromJwk = new keyutil.Key("jwk", publicKey);
         let keyPrivateObjFromJwk = new keyutil.Key("jwk", privateKey);
 
-        //Posteriormente las pasamos de JWK a PEM para poder guardarlas en un archivo de texto
+        //posteriormente las pasamos de JWK a PEM para poder guardarlas en un archivo de texto
         let pemPublic = await keyPublicObjFromJwk.export("pem");
         let pemPrivate = await keyPrivateObjFromJwk.export("pem");
 
-        //console.log(pemPublic);
-        //console.log(pemPrivate);
-
         this.llavePemPublicaGenerada = pemPublic;
         this.llavePemPrivadaGenerada = pemPrivate;
-
-        //this.crearArchivo("publica.key", pemPublic);
-        //this.crearArchivo("privada.key", pemPrivate);
       });
     },
 
-    crearArchivo(nombreArchivo, texto) {
+    /**
+     * funcion crearArchivoTxt()
+     * Crea un archivo dado un nombre y su contenido en forma de texto,
+     * el cual es guardado de forma automatica en la carpeta de descargas del usuario
+     * @param {String} nombreArchivo es el nombre del archivo
+     * @param {String} texto es el contenido del archivo
+     */
+    crearArchivoTxt(nombreArchivo, texto) {
       var element = document.createElement("a");
       element.setAttribute(
         "href",
@@ -290,6 +315,15 @@ export default {
       document.body.removeChild(element);
     },
 
+    /**
+     * funcion generarArchivo()
+     *
+     * Crea un archivo dado un nombre y su contenido en forma de blob,
+     * el cual es guardado de forma automatica en la carpeta de Descargas del usuario
+     *
+     * @param {String} nombreArchivo es el nombre del archivo
+     * @param {Blob} contenido es el contenido (binarios) del archivo
+     */
     generarArchivo(nombreArchivo, contenido) {
       let element = document.createElement("a");
 
@@ -305,17 +339,22 @@ export default {
       document.body.removeChild(element);
     },
 
-    verificarCampos() {
+    /**
+     * funcion verificarCamposFirma()
+     *
+     * Verifica que todos los campos del form de la dirma digital esten llenos para
+     * despues empezar a hacer la firma digital
+     */
+    verificarCamposFirma() {
       if (this.$refs.formFirma.validate()) {
-        console.log("lleno");
         this.firmarArchivo();
       }
     },
 
     /**
-     * funcion leerArchivo()
+     * funcion leerArchivoOriginal()
      *
-     * funcion que se encarga de leer y obtener los datos del archivo de entrada
+     * funcion que se encarga de leer y obtener los datos del archivo a firmar
      * cada vez que es proporcionado por el usuario
      * @param {File} archivo es el archivo proporcionado por el usuario desde el
      * <v-file-input/>
@@ -333,8 +372,7 @@ export default {
 
       /**
        * finalmente, el fileReader se encarga de obtener el contenido del
-       * archivo en forma de un arrayBuffer el cual es guardado en contenidoArchivo
-       * el cual se usara posteriormente
+       * archivo en forma de un arrayBuffer el cual es guardado en archivoOriginal
        */
       let reader = new FileReader();
       reader.readAsArrayBuffer(archivo);
@@ -343,6 +381,16 @@ export default {
       };
     },
 
+    /**
+     * funcion leerLlavePrivada()
+     *
+     * funcion que se encarga de leer una llave privada en formato PEM proporcionada
+     * por el usuario,la cual se convierte a formato JWK para que la libreria js-crypto-rsa
+     * pueda usarla
+     *
+     * @param {File} archivo es el archivo que contiene la llave privada, la cual es
+     * proporcionada por el usuario desde el <v-file-input/>
+     */
     leerLlavePrivada(archivo) {
       //validacion de archivo de entrada
       if (!archivo) {
@@ -350,49 +398,51 @@ export default {
         return;
       }
 
-      /**
-       * finalmente, el fileReader se encarga de obtener el contenido del
-       * archivo en forma de un arrayBuffer el cual es guardado en contenidoArchivo
-       * el cual se usara posteriormente
-       */
       let reader = new FileReader();
       reader.readAsText(archivo);
       reader.onload = () => {
+        //una vez leido el contenido de la llave la guardamos en formato PEM y JWK
         this.llavePemPrivada = reader.result;
-        console.log(reader.result);
         this.convertirPEMaJWK(reader.result).then(
           (jwk) => (this.llavePrivada = jwk)
         );
       };
     },
 
+    /**
+     * funcion firmarArchivo()
+     * 
+     * Crea la firma digital del archivo de entrada con RSA, utilizando la llave privada proporcionada por el usuario.
+     * Una vez creada la firma esta se concatena con el contenido del archivo de entrada para crear el nuevo 
+     * archivo firmado el cual sera descargado por el usuario
+     * 
+     */
     firmarArchivo() {
-      rsa
-        .sign(this.archivoOriginal, this.llavePrivada, "SHA-256", {
-          // optional
-          name: "RSA-PSS", // default. 'RSASSA-PKCS1-v1_5' is also available.
+      //firmamos el archivo usando SHA-2
+      rsa.sign(this.archivoOriginal, this.llavePrivada, "SHA-256", {
+          name: "RSA-PSS",
           saltLength: 64,
         })
         .then((firma) => {
-          console.log("firmado");
-          console.log(firma);
+          //una vez generada la firma, se pasa a base64 para concatenarla al nuevo archivo
           let firmaBase64 = encode(firma);
+          //posteriormente se crea el archivo firmado, el cual contiene el mensaje original, un delimitador y la firma digital
           let contenidoArchivo = new Blob(
             [this.archivoOriginal, "\n-#-#-#FIRMA-#-#-#\n", firmaBase64],
             {
               type: "text/plain",
             }
           );
-          console.log(contenidoArchivo);
           this.generarArchivo(this.nombreArchivo + "_s.txt", contenidoArchivo);
         });
     },
 
     /**
-     * funcion leerArchivo()
+     * funcion leerArchivoFirmado()
      *
-     * funcion que se encarga de leer y obtener los datos del archivo de entrada
-     * cada vez que es proporcionado por el usuario
+     * funcion que se encarga de leer y obtener los datos (mensaje y firma) del archivo firmado
+     * por separado
+     * 
      * @param {File} archivo es el archivo proporcionado por el usuario desde el
      * <v-file-input/>
      */
@@ -403,22 +453,27 @@ export default {
         return;
       }
 
-      /**
-       * finalmente, el fileReader se encarga de obtener el contenido del
-       * archivo en forma de un arrayBuffer el cual es guardado en contenidoArchivo
-       * el cual se usara posteriormente
-       */
       let reader = new FileReader();
       reader.readAsText(archivo);
       reader.onload = () => {
         let input = reader.result;
+        //separamos el archivo con el delimitador y de esa forma obtenemos el mensaje y la firma del archivo por separado
         let partes = input.split("\n-#-#-#FIRMA-#-#-#\n");
         this.archivoFirmado = new TextEncoder("utf-8").encode(partes[0]);
         this.firmaArchivo = decode(partes[1]);
-        console.log(this.firmaArchivo);
       };
     },
 
+    /**
+     * funcion leerLlavePublica()
+     *
+     * funcion que se encarga de leer la llave publica en formato PEM proporcionada
+     * por el usuario,la cual se convierte a formato JWK para que la libreria js-crypto-rsa
+     * pueda usarla
+     *
+     * @param {File} archivo es el archivo que contiene la llave publica, la cual es
+     * proporcionada por el usuario desde el <v-file-input/>
+     */
     leerLlavePublica(archivo) {
       //validacion de archivo de entrada
       if (!archivo) {
@@ -426,52 +481,72 @@ export default {
         return;
       }
 
-      /**
-       * finalmente, el fileReader se encarga de obtener el contenido del
-       * archivo en forma de un arrayBuffer el cual es guardado en contenidoArchivo
-       * el cual se usara posteriormente
-       */
       let reader = new FileReader();
       reader.readAsText(archivo);
       reader.onload = () => {
+        //una vez leido el contenido de la llave la guardamos en formato PEM y JWK
         this.llavePemPublica = reader.result;
-        console.log(reader.result);
         this.convertirPEMaJWK(reader.result).then(
           (jwk) => (this.llavePublica = jwk)
         );
       };
     },
 
+    /**
+     * funcion validarCamposVerificacion()
+     *
+     * Asegura que todos los campos del form de la verificacion esten llenos para
+     * despues empezar a hacer la verificacion del archivo firmado
+     */
     validarCamposVerificacion() {
       if (this.$refs.formVerificacion.validate()) {
-        console.log("lleno");
         this.verificarArchivo();
       }
     },
 
+    /**
+     * funcion verificarArchivo()
+     * 
+     * Funcion que se encarga de comprobar la firma digital de un archivo dado con RSA.
+     * 
+     * Para llevar a cabo la verificacion, se utiliza el mensaje original, la firma del archivo y 
+     * la llave publica del emisor.
+     * 
+     * Dependiendo del resultado, se le notificara al usuario si la firma es valida y si el archivo 
+     * ha sido modificado despues de ser firmado
+     * 
+     */
     verificarArchivo() {
-      rsa
-        .verify(
-          this.archivoFirmado,
-          this.firmaArchivo,
-          this.llavePublica,
-          "SHA-256",
-          {
-            // optional
-            name: "RSA-PSS", // default. 'RSASSA-PKCS1-v1_5' is also available.
-            saltLength: 64, // default is the same as hash length
+      //validamos usando SHA-2
+      rsa.verify(this.archivoFirmado, this.firmaArchivo, this.llavePublica, "SHA-256", {
+            name: "RSA-PSS", 
+            saltLength: 64,
           }
         )
         .then((valid) => {
+          /**
+           * Dependiendo del resultado, se le notificara al usuario si la firma es valida y si el archivo 
+           * ha sido modificado despues de ser firmado
+           */
           this.snackbar = true;
-          this.infoSnackbar = valid ? "La firma proporcionada en el archivo es valida" : "La firma o mensaje proporcionado en el archivo no son validos";
-          this.colorSnackbar = valid ? "green accent-4" : "error"
+          this.infoSnackbar = valid
+            ? "La firma proporcionada en el archivo es valida"
+            : "La firma no es valida. El archivo ha sido modificado despues de ser firmado";
+          this.colorSnackbar = valid ? "green accent-4" : "error";
         });
     },
 
+    /**
+     * funcion convertirPEMaJWK()
+     * 
+     * funcion asincrona que convierte una llave con formato PEM a JKW
+     * 
+     * @param {String} stringPem es la llave en formato PEM
+     * @returns {Promise} es una promesa que tendra como resultado la llave en formato JWK
+     */
     async convertirPEMaJWK(stringPem) {
       const keyObjFromPem = new keyutil.Key("pem", stringPem);
-      return await keyObjFromPem.export("jwk"); // export public key from private key.
+      return await keyObjFromPem.export("jwk");
     },
   },
 };
